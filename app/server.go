6ec2746759c1dedce5479bd69fc9414577626612
fatty_ray <serv_me_mail@proton.me>
2023-04-22 20:24:13 +0300

    Second task

--- app/server.go
+++ app/server.go
@@ -2,27 +2,50 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
+	"strings"
 	// Uncomment this block to pass the first stage
 	// "net"
 	// "os"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
 
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
+
+	request, err := io.ReadAll(conn)
+	strings := strings.Split(string(request), " ")
+	if len(strings) > 2 {
+		writeErr(&conn)
+	}
+
+	if strings[0] == "PING" {
+		if len(strings) == 1 {
+			writeOk(&conn, "PONG")
+		} else {
+			writeOk(&conn, strings[1])
+		}
+	}
+
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+}
+
+func writeOk(c *net.Conn, arg string) {
+	responseFormat := "+%s\r\n"
+	io.WriteString(*c, fmt.Sprintf(responseFormat, arg))
+}
+
+func writeErr(conn *net.Conn) {
+	io.WriteString(*conn, "-ERR Too many arguments\r\n")
 }
